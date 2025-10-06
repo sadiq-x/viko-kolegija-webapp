a1f3127cb7c7d1916300cc2abab1a8d96003d678
Rafael Alves <sadiqmasterdevil@gmail.com>
2025-10-06 14:25:23 +0300

    Adding new pages at project

--- frontend-angular/frontend-app/src/app/app.routes.ts
+++ frontend-angular/frontend-app/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { Auth } from './services/auth';
-import { Home } from './pages/private/home/home';
+import { Home } from './pages/home/home';
 import { Profile } from './pages/private/profile/profile';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 import { Courses } from './pages/private/courses/courses';
 import { Layout } from './pages/private/layout/layout';
+import { Dashboard } from './pages/private/dashboard/dashboard';
 
 export const routes: Routes = [
     { path: '', redirectTo: '', pathMatch: 'full' },
@@ -13,8 +14,8 @@ export const routes: Routes = [
         path: '', component: Layout, canActivate: [Auth],
         children: [
             {
-                path: 'home',
-                component: Home
+                path: 'dashboard',
+                component: Dashboard
             },
             {
                 path: 'profile',
@@ -28,5 +29,6 @@ export const routes: Routes = [
     },
     { path: 'login', component: Login },
     { path: 'register', component: Register },
+    { path: 'home', component: Login },
     { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
---
+++ frontend-angular/frontend-app/src/app/pages/private/courseslist/courseslist.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courseslist',
+  imports: [],
+  templateUrl: './courseslist.html',
+  styleUrl: './courseslist.scss'
+})
+export class Courseslist {
+
+}
