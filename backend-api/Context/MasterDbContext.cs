fb30861fa3f56734f6f6341895c4e2778b92749c
Rafael Alves <sadiqmasterdevil@gmail.com>
2025-11-22 19:03:58 +0200

    Adding some changes at code

--- backend-api/Context/MasterDbContext.cs
+++ backend-api/Context/MasterDbContext.cs
@@ -61,119 +61,119 @@ namespace backend_api.Context
     public class Entities
     { //Class Entities
         [Key]
-        public int Id { get; set; } //Primary key
+        public int Id { get; set; } 
         [Required, MaxLength(100)]
-        public string Name { get; set; } = null!; //Name of entity
+        public string Name { get; set; } = null!; 
         [Required, MaxLength(150)]
         [EmailAddress]
-        public string Email { get; set; } = null!; //Email of entity
-        public string? Image { get; set; } //Image of entity
+        public string Email { get; set; } = null!; 
+        public string? Image { get; set; }
         [MaxLength(20)]
         [Phone]
-        public string NumberPhone { get; set; } //Number phone of entity
+        public string NumberPhone { get; set; } 
         [MaxLength(200)]
-        public string Address { get; set; } //Address of entity
+        public string Address { get; set; }
         [MaxLength(20)]
-        public string? Birthday { get; set; } //Gender of entity
+        public string? Birthday { get; set; } 
         [MaxLength(20)]
-        public string? Nationality { get; set; } //Gender of entity
+        public string? Nationality { get; set; } 
         [MaxLength(20)]
-        public string? Gender { get; set; } //Gender of entity
-        public int RoleId { get; set; } //Type role of entity
+        public string? Gender { get; set; } 
+        public int RoleId { get; set; } 
         [ForeignKey("RoleId")]
-        public Roles Roles { get; set; } //Relationship with id of table Roles
+        public Roles Roles { get; set; }
         public ICollection<ParticipantsEvents> ParticipantsEvents { get; set; } = new List<ParticipantsEvents>();
         public ICollection<Events> Events { get; set; } = new List<Events>();
     }
 
-    [Table("Roles")] //Maps the class Roles for table Roles in Database
+    [Table("Roles")] 
     [Index(nameof(Type), IsUnique = true)]
     public class Roles
     {
         [Key]
-        public int Id { get; set; } //Primary key
+        public int Id { get; set; } 
         [Required]
-        public string Type { get; set; } //Type of role
+        public string Type { get; set; }  
     }
 
-    [Table("Users")]  //Maps the class Users for table Users in Database
+    [Table("Users")]  
     [Index(nameof(Username), IsUnique = true)]
     public class Users
     {
         [Key]
-        public int Id { get; set; } //Id of User
+        public int Id { get; set; }
         [Required]
-        public int EntityId { get; set; }  //Entity_id of User
+        public int EntityId { get; set; }  
         [ForeignKey("EntityId")]
-        public Entities Entity { get; set; } //Relationship with id of table Entities
+        public Entities Entity { get; set; }  
         [Required]
         [MaxLength(50)]
-        public string Username { get; set; } //Username of User, required
+        public string Username { get; set; }  
         [Required]
         [JsonIgnore]
-        public string PasswordHash { get; set; } //Password of User, required and jsonIgnore
+        public string PasswordHash { get; set; } 
     }
 
-    [Table("Events")] //Maps the class Events for table Events in Database
+    [Table("Events")]  
     public class Events
     { 
         [Key]
-        public int Id { get; set; } //Primary key
+        public int Id { get; set; }  
         [Required]
-        public string Name { get; set; } //Name of event
-        public string Description { get; set; } //Description of event
-        public int TopicsId { get; set; } //Type Topic of event
+        public string Name { get; set; }  
+        public string Description { get; set; }  
+        public int TopicsId { get; set; }  
         [ForeignKey("TopicsId")]
-        public Topics Topics { get; set; } //Relationship with id of table Roles
-        public int CreateById { get; set; } //Event created by id?
+        public Topics Topics { get; set; }  
+        public int CreateById { get; set; }  
         [ForeignKey("CreateById")]
-        public Entities Entities { get; set; } //Relationship with id of table Entities
-        public string DateCreate { get; set; } //Event created in date
-        public string? DateClose { get; set; } //Event created in date
-        public int StatusId { get; set; } //Status of event, have three types of status
+        public Entities Entities { get; set; } 
+        public string DateCreate { get; set; } 
+        public string? DateClose { get; set; } 
+        public int StatusId { get; set; }
         [ForeignKey("StatusId")]    
-        public StatusEvents StatusEvents { get; set; } //Relationship with id of table Entities
+        public StatusEvents StatusEvents { get; set; } 
         public ICollection<ParticipantsEvents> ParticipantsEvents { get; set; } = new List<ParticipantsEvents>();
     }
 
-    [Table("ParticipantsEvents")] //Maps the class ParticipantsEvents for table ParticipantsEvents in Database
+    [Table("ParticipantsEvents")] 
     public class ParticipantsEvents
     { 
         [Key]
-        public int Id { get; set; } //Primary key
-        public int EventId { get; set; } //Event id?
+        public int Id { get; set; }  
+        public int EventId { get; set; }  
         [ForeignKey("EventId")]
-        public Events Event { get; set; } = null!; //Relationship with id of table Events
-        public int EntityId { get; set; } //Entity id?
+        public Events Event { get; set; } = null!;  
+        public int EntityId { get; set; }  
         [ForeignKey("EntityId")]
-        public Entities Entity { get; set; } //Relationship with id of table Entities
-        public bool Status { get; set; }//Status of participant in event, true represent inscription on event, false represent withdrawal 
-        public string? Grade { get; set; } //Grade in 1 event of 1 participant
-        public string? Comments { get; set; } //Comment of participant in some event
-        public string? ParticipantDescription { get; set; } //Text/Write area of participant in some event
+        public Entities Entity { get; set; } 
+        public bool Status { get; set; }
+        public string? Grade { get; set; }  
+        public string? Comments { get; set; }  
+        public string? ParticipantDescription { get; set; }  
     }
 
-    [Table("Topics")] //Maps the class Topics for table Topics in Database
+    [Table("Topics")] 
     [Index(nameof(Type), IsUnique = true)]
     public class Topics
     { 
         [Key]
-        public int Id { get; set; } //Primary key
+        public int Id { get; set; } 
         [Required]
-        public string Type { get; set; } = null!; //Type of Topic
+        public string Type { get; set; } = null!; 
         [Required]
-        public string Description { get; set; } = null!; //Description of Topic
+        public string Description { get; set; } = null!; 
         public ICollection<Events> Events { get; set; } = new List<Events>();
     }
 
-    [Table("StatusEvents")] //Maps the class StatusEvents for table StatusEvents in Database
+    [Table("StatusEvents")] 
     [Index(nameof(Type), IsUnique = true)]
     public class StatusEvents
     { 
         [Key]
-        public int Id { get; set; } //Primary key
+        public int Id { get; set; } 
         [Required]
-        public string Type { get; set; } = null!; //Type of Topic
+        public string Type { get; set; } = null!; 
         public ICollection<Events> Events { get; set; } = new List<Events>();
     }
 }
